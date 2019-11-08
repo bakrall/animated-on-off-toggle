78c3aa9e40775156945f210bd51393bd6f3b0aa3
Barbara Stadnik <barbara1stadnik@gmail.com>
2019-11-08 22:35:12 +0100

    make toggle accessible

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-$('input').click(function() {
+$('input').click(() => {
 	$('body').toggleClass('on');
+	$('.on').length ? $('.visually-hidden').text('on') : $('.visually-hidden').text('off');
+});
+
+/*event.which and event.keyCode are depracated
+event.key should be used instead
+https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent
+*/
+
+// make toggle switch on/off when pressing Enter or Space
+$('.container').keydown((event) => {
+	if (event.key === "Enter" || event.key === " ") {
+		$('input').trigger('click');
+	}
 });
